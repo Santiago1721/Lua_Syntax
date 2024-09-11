708feabcdf511f5067d5248d495fe88eba8f50c6
Santiago Gutierrez <119289025+Santiago1721@users.noreply.github.com>
2024-09-11 20:13:17 +0200

    Create test1.lua

---
+++ test1.lua
@@ -0,0 +1,48 @@
+print("Vinted 2 :v")
+
+
+
+
+precio_prenda1 = 5
+precio_prenda2 = 12
+cantidad_de_prendas1 = 34
+cantidad_de_prendas2 = 29
+
+print("el beneficio total por la venta de las prendas de tipo 1 seria:")
+
+print(precio_prenda1 * cantidad_de_prendas1)
+
+
+
+
+print("el beneficio total por la venta de las prendas de tipo 2 seria:")
+
+print(precio_prenda2 * cantidad_de_prendas2)
+
+
+
+num_departamentos = 8
+metros_de_cocina = 8
+metros_de_comedor = 15
+
+print("la cantidad total de valdosa a poner seria:")
+
+print((metros_de_cocina + metros_de_comedor) * num_departamentos)
+
+print("aqui vamos a utilizar el scanner de lua")
+
+--el scanner en lua se llama con la funcion = io.read()
+
+precio_silla = io.read()
+cantidad_sillas = io.read()
+
+total_beneficio = precio_silla * cantidad_sillas
+
+print('el total de la venta es ',total_beneficio)
+
+
+-------------------------------------------------------------------------
+
+--Valores Booleanos:
+
+
